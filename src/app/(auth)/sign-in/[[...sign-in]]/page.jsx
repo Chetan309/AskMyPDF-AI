4c9651dc501e90dcd254a0e0420f1f603342bf49
Chetan309 <Chetan309044@gmail.com>
2025-05-05 16:11:11 +0530

    first commit

--- src/app/(auth)/sign-in/[[...sign-in]]/page.jsx
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -1,9 +1,69 @@
-import { SignIn } from '@clerk/nextjs'
+import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <div className="flex items-center justify-center h-screen">
-        <SignIn />
-    </div>
-)
+    <section className="bg-white">
+      <div className="lg:grid lg:min-h-screen lg:grid-cols-12">
+        <section className="relative flex h-32 items-end bg-gray-900 lg:col-span-5 lg:h-full xl:col-span-6">
+          <img
+            alt=""
+            src="/hp3.jpg"
+            className="absolute inset-0 h-full w-full object-cover opacity-80"
+          />
+
+          <div className="hidden lg:relative lg:block lg:p-12">
+           
+
+            <h2 className="mt-6 text-2xl font-bold text-white sm:text-3xl md:text-4xl">
+              Welcome to AskMyPDF AI
+            </h2>
+
+            <p className="mt-4 leading-relaxed text-white/90">
+            "Explore the power of your PDFs with AskMyPDF AI — your smart assistant for instant document insights, answers, and understanding."
+            </p>
+          </div>
+        </section>
+
+        <main className="flex items-center justify-center px-8 py-8 sm:px-12 lg:col-span-7 lg:px-16 lg:py-12 xl:col-span-6 bg-black">
+          <div className="max-w-xl lg:max-w-3xl">
+            <div className="relative -mt-16 block lg:hidden">
+              <a
+                className="inline-flex size-16 items-center justify-center rounded-full bg-white text-blue-600 sm:size-20"
+                href="#"
+              >
+                <span className="sr-only">Home</span>
+                
+              </a>
+
+              <h1 className="mt-2 text-2xl font-bold text-white sm:text-3xl md:text-4xl">
+              Welcome to AskMyPDF AI
+              </h1>
+
+              <p className="mt-4 leading-relaxed text-gray-500 mb-10 text-justify">
+              "Explore the power of your PDFs with AskMyPDF AI — your smart assistant for instant document insights, answers, and understanding."
+
+              </p>
+            </div>
+
+            <SignIn
+              appearance={{
+                elements: {
+                  formButtonPrimary: {
+                    backgroundColor: "#22c55e",
+                    color: "#fff",
+                    padding: "10px 20px",
+                    borderRadius: "5px",
+                    border: "none",
+                    ":hover": {
+                      backgroundColor: "#16a34a",
+                    },
+                  },
+                },
+              }}
+            />
+          </div>
+        </main>
+      </div>
+    </section>
+  );
 }
--- src/app/(auth)/sign-up/[[...sign-up]]/page.jsx
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.jsx
@@ -1,9 +1,69 @@
 import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
-    return (
-        <div className="flex items-center justify-center h-screen">
-            <SignUp />
-        </div>
-    );
+  return (
+    <section className="bg-white">
+      <div className="lg:grid lg:min-h-screen lg:grid-cols-12">
+        <section className="relative flex h-32 items-end bg-gray-900 lg:col-span-5 lg:h-full xl:col-span-6">
+          <img
+            alt=""
+            src="/hp4.jpg"
+            className="absolute inset-0 h-full w-full object-cover opacity-80"
+          />
+
+          <div className="hidden lg:relative lg:block lg:p-12">
+            <a className="block text-white" href="#">
+            </a>
+
+            <h2 className="mt-6 text-2xl font-bold text-white sm:text-3xl md:text-4xl">
+              Register to AskMyPDF AI
+            </h2>
+
+            <p className="mt-4 leading-relaxed text-white/90">
+            "Join AskMyPDF AI and experience a smarter way to interact with your documents — ask, understand, and learn from your PDFs instantly."
+            </p>
+          </div>
+        </section>
+
+        <main className="flex items-center justify-center px-8 py-8 sm:px-12 lg:col-span-7 lg:px-16 lg:py-12 xl:col-span-6 bg-black">
+          <div className="max-w-xl lg:max-w-3xl">
+            <div className="relative -mt-16 block lg:hidden">
+              <a
+                className="inline-flex size-16 items-center justify-center rounded-full bg-white text-blue-600 sm:size-20"
+                href="#"
+              >
+                <span className="sr-only">Home</span>
+              
+              </a>
+
+              <h1 className="mt-2 text-2xl font-bold text-white sm:text-3xl md:text-4xl">
+              Register to AskMyPDF AI
+              </h1>
+
+              <p className="mt-4 leading-relaxed text-gray-500 mb-10 text-justify">
+              "Join AskMyPDF AI and experience a smarter way to interact with your documents — ask, understand, and learn from your PDFs instantly."
+              </p>
+            </div>
+
+            <SignUp
+              appearance={{
+                elements: {
+                  formButtonPrimary: {
+                    backgroundColor: "#22c55e",
+                    color: "#fff",
+                    padding: "10px 20px",
+                    borderRadius: "5px",
+                    border: "none",
+                    ":hover": {
+                      backgroundColor: "#16a34a",
+                    },
+                  },
+                },
+              }}
+            />
+          </div>
+        </main>
+      </div>
+    </section>
+  );
 }
